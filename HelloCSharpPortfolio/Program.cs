5623386185c77655815c8bb4c5ab64ca71c74576
Godfred <goddysmart2468@gmail.com>
2025-08-27 04:12:56 +0000

    Hello C#

---
+++ HelloCSharpPortfolio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // asking for the user's input
+        Console.Write("What is your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        string ageInput = Console.ReadLine();
+
+        int age = int.Parse(ageInput);
+
+        Console.WriteLine($"\nHello, {name}! You are {age} years old.");
+        Console.WriteLine("Welcome to the world of thegoddysmart!");
+    }
+}
